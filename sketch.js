29f308076cca019c63c1bef920ca5984f478df16
XyuiopGameGuy <92894184+XyuiopGameGuy@users.noreply.github.com>
2021-11-01 19:09:01 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,17 +5,18 @@ var sea;
 
 function preload(){
 sea = loadImage("sea.png");
-navioMovendo = loadAnimation("ship-1.png, ship-2.png, ship-3.png, ship-4.png");
+navioMovendo = loadAnimation("ship-1.png", "ship-2.png", "ship-3.png", "ship-4.png");
 }
 
 function setup(){
   createCanvas(400,400);
-  navio= createSprite(100, 200, 80, 80);
-  navio.addAnimation(navioMovendo);
-  navio.scale = 0.5
 
   mar= createSprite(200, 100, 80, 80);
   mar.addImage(sea);
+
+  navio= createSprite(100, 200, 80, 80);
+  navio.addAnimation("movendo", navioMovendo);
+  navio.scale = 0.25
 }
 
 function draw() {
