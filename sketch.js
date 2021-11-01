1d3447e518514a068e3985e09521db0fbda65d20
XyuiopGameGuy <92894184+XyuiopGameGuy@users.noreply.github.com>
2021-11-01 18:33:28 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var navio;
+var navioMovendo;
+var mar;
+var sea;
+
+function preload(){
+sea = loadImage("sea.png");
+navioMovendo = loadAnimation("ship-1.png, ship-2.png, ship-3.png, ship-4.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  navio= createSprite(100, 200, 80, 80);
+  navio.addAnimation(navioMovendo);
+  navio.scale = 0.5
+
+  mar= createSprite(200, 100, 80, 80);
+  mar.addImage(sea);
+}
+
+function draw() {
+  background("blue");
+  mar.velocityX= -5
+  console.log(navio.y);
+ if (mar.x < 0){
+   mar.x = mar.width/2;
+  }
+    drawSprites();
+
+ 
+}
